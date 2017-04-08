0fd7db0d9bafd53b6bb0b915d6e70a0d39449e11
Atmelfan <gustavp@gpa-robotics.com>
2017-04-08 22:53:05 +0200

    USB..?

---
+++ hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/Descriptors.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/Descriptors.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Common/HIDParser.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Common/HIDParser.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Device/AudioClassDevice.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Device/AudioClassDevice.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Device/CDCClassDevice.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Device/CDCClassDevice.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Device/HIDClassDevice.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Device/HIDClassDevice.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Device/MIDIClassDevice.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Device/MIDIClassDevice.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Device/MassStorageClassDevice.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Device/MassStorageClassDevice.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Device/PrinterClassDevice.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Device/PrinterClassDevice.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Device/RNDISClassDevice.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Device/RNDISClassDevice.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Host/AndroidAccessoryClassHost.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Host/AndroidAccessoryClassHost.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Host/AudioClassHost.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Host/AudioClassHost.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Host/CDCClassHost.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Host/CDCClassHost.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Host/HIDClassHost.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Host/HIDClassHost.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Host/MIDIClassHost.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Host/MIDIClassHost.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Host/MassStorageClassHost.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Host/MassStorageClassHost.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Host/PrinterClassHost.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Host/PrinterClassHost.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Host/RNDISClassHost.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Host/RNDISClassHost.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Class/Host/StillImageClassHost.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Class/Host/StillImageClassHost.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/ConfigDescriptors.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/ConfigDescriptors.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/DeviceStandardReq.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/DeviceStandardReq.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/Events.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/Events.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/HostStandardReq.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/HostStandardReq.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/USBTask.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/USBTask.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/XMEGA/Device_XMEGA.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/XMEGA/Device_XMEGA.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/XMEGA/EndpointStream_XMEGA.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/XMEGA/EndpointStream_XMEGA.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/XMEGA/Endpoint_XMEGA.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/XMEGA/Endpoint_XMEGA.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/XMEGA/Host_XMEGA.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/XMEGA/Host_XMEGA.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/XMEGA/PipeStream_XMEGA.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/XMEGA/PipeStream_XMEGA.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/XMEGA/Pipe_XMEGA.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/XMEGA/Pipe_XMEGA.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/XMEGA/USBController_XMEGA.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/XMEGA/USBController_XMEGA.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/LUFA/Drivers/USB/Core/XMEGA/USBInterrupt_XMEGA.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/LUFA/Drivers/USB/Core/XMEGA/USBInterrupt_XMEGA.c.o"
+  "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/VirtualSerial.c" "/home/atmelfan/documents/Kandidat/hbridge/goldsrc/cmake-build-debug/CMakeFiles/avr-clion.dir/VirtualSerial.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ARCH=ARCH_XMEGA"
+  "ARCH=XMEGA"
+  "BOARD=USER"
+  "F_USB=48000000"
+  "PID=ffff"
+  "VID=9999"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../LUFA/Drivers"
+  "../LUFA/Drivers/Board"
+  "../LUFA/USB"
+  "../Config"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hbridge/goldsrc/cmake-build-debug/CMakeFiles/goldsrc.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for goldsrc.
+# This may be replaced when dependencies are built.
